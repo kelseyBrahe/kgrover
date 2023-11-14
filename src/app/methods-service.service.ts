a5d0fda351f202a9304f712f42629384bce239d4
Kelsey Brahe Grover <kgrover3@myune.edu.au>
2023-11-14 12:01:19 -0700

    ng-cloak edits and methods-service edit

--- src/app/methods-service.service.ts
+++ src/app/methods-service.service.ts
@@ -20,7 +20,6 @@ export class MethodsService {
   }
 
   ngOnInit() {
-    this.spinner();
   }
 
   /**
@@ -82,34 +81,6 @@ export class MethodsService {
         }
       }
     }
-  }
-
-  spinner() {
-    const width = window.innerWidth;
-    var TO = 20;
-    this.text = document.querySelector('.text p');
-
-    if (width > 913 && width < 1199) {
-        TO = 25;
-    } else if (width > 767 && width <= 913) {
-        TO = 26;
-    } else if (width > 500 && width <= 767) {
-        TO = 35;
-    } else if (width > 0 && width <= 500) {
-        TO = 45;
-    } else {
-        TO = 20;
-    }
-
-    this.text.innerHTML = this.text.innerText.split("").map(
-        (char:any, i:any) => 
-            `<span style="
-                transform: rotate(${i * 6}deg); 
-                position: absolute; 
-                left: 50%;
-                transform-origin: 0 ${TO}vw;
-            ">${char}</span>`
-    ).join("");
   }
 }
 
